52f35e4ec183813a3eb8e2fe1a10b10cc7dc464a
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-03-29 16:39:51 +0100

    glt2024: more progs

--- bin/samples.py
+++ bin/samples.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from blink.glt2024 import GLTMatrix, GLTButtons, blink_rows, blink_cols, cycle_cols, spiral, mad, wait_button, mad_spiral_until_left_button
+from blink.glt2024 import GLTMatrix, GLTButtons, blink_rows, blink_cols, cycle_cols, spiral, mad, wait_button, mad_spiral_until_left_button, blackhole, blackhole_spiral, mickey, all_directions
 from blink.blink import launch_isolated, launch, walk, on, sleep, any, all, forever, sequence, blink, TASK_GROUP, program, repeat, cycle
 from blink.io import Output
 
@@ -11,181 +11,13 @@ import contextvars
 buttons = GLTButtons()
 matrix = GLTMatrix()
 
-@program
-async def mickey(matrix, left_ear, right_ear):
-    left_eye_full = Output.from_many(
-        (
-            matrix.get(0,0),
-            matrix.get(0,1),
-            matrix.get(0,2),
-            matrix.get(1,0),
-            matrix.get(1,1),
-            matrix.get(1,2),
-            matrix.get(2,0),
-            matrix.get(2,1),
-            matrix.get(2,2),
-        ))
-    left_eye_cross = Output.from_many(
-        (
-            matrix.get(0,0),
-            matrix.get(0,2),
-            matrix.get(1,1),
-            matrix.get(2,0),
-            matrix.get(2,2),
-        ))
-    left_eye_dot = matrix.get(1,1)
-    right_eye_full = Output.from_many(
-        (
-            matrix.get(0,2),
-            matrix.get(0,3),
-            matrix.get(0,4),
-            matrix.get(1,2),
-            matrix.get(1,3),
-            matrix.get(1,4),
-            matrix.get(2,2),
-            matrix.get(2,3),
-            matrix.get(2,4),
-        ))
-    right_eye_cross = Output.from_many(
-        (
-            matrix.get(0,2),
-            matrix.get(0,4),
-            matrix.get(1,3),
-            matrix.get(2,2),
-            matrix.get(2,4),
-        ))
-    right_eye_dot = matrix.get(1, 3)
-
-    mouth_up = Output.from_many(
-        (
-            matrix.get(3,0),
-            matrix.get(4,1),
-            matrix.get(4,2),
-            matrix.get(4,3),
-            matrix.get(3,4),
-        ))
-    mouth_straight = Output.from_many(
-        (
-            matrix.get(4,0),
-            matrix.get(4,1),
-            matrix.get(4,2),
-            matrix.get(4,3),
-            matrix.get(4,4),
-        ))
-    mouth_down = Output.from_many(
-        (
-            matrix.get(4,0),
-            matrix.get(3,1),
-            matrix.get(3,2),
-            matrix.get(3,3),
-            matrix.get(4,4),
-        ))
-
-    await launch(
-        sequence(
-            any(
-                on(mouth_up),
-                any(
-                    repeat(
-                        sequence(                
-                            any(
-                                blink(left_eye_full, interval=0.5),
-                                wait_button(left_ear),
-                            ),
-                            any(
-                                blink(left_eye_dot, interval=0.5),
-                                wait_button(left_ear),
-                            ),
-                        ),
-                        ntimes=3
-                    ),
-                    repeat(
-                        sequence(                
-                            sleep(0.5),
-                            any(
-                                blink(right_eye_full, interval=0.5),
-                                wait_button(right_ear),
-                            ),
-                            any(
-                                blink(right_eye_dot, interval=0.5),
-                                wait_button(right_ear),
-                            ),
-                        ),
-                        ntimes=3
-                    ),
-                ),
-            ),
-            any(
-                on(mouth_straight),
-                all(
-                    any(
-                        on(left_eye_dot),
-                        wait_button(left_ear),
-                    ),
-                    any(
-                        on(right_eye_dot),
-                        wait_button(right_ear),
-                    ),
-                ),
-            ),
-            any(
-                on(mouth_straight),
-                all(
-                    on(left_eye_cross),
-                    on(right_eye_cross),
-                ),
-                all(
-                    wait_button(left_ear),
-                    wait_button(right_ear),
-                )
-            ),
-            all(
-                on(left_eye_cross),
-                on(right_eye_cross),
-                on(mouth_down),
-            ),
-        )
-    )
-
-@program
-async def all_directions(matrix, left_ear, right_ear):
-    await launch(
-        all(
-            forever(
-                sequence(
-                    any(
-                        cycle(matrix.outer_ring_clockwise(), interval=0.1),
-                        wait_button(left_ear),
-                    ),
-                    any(
-                        cycle(list(reversed(matrix.outer_ring_clockwise())), interval=0.1),
-                        wait_button(left_ear),
-                    ),
-                )
-            ),
-            forever(
-                sequence(
-                    any(
-                        cycle(matrix.inner_ring_clockwise(), interval=0.2),
-                        wait_button(right_ear),
-                    ),
-                    any(
-                        cycle(list(reversed(matrix.inner_ring_clockwise())), interval=0.2),
-                        wait_button(right_ear),
-                    ),
-                )
-            ),
-        )
-    )
-
-    
-
 async def main():
-    # prog = mad_spiral_until_left_button(matrix=matrix, buttons=buttons)
-    prog = mickey(matrix=matrix, left_ear=buttons.left, right_ear=buttons.right)
-    # prog = all_directions(matrix=matrix, left_ear=buttons.left, right_ear=buttons.right)
-    # prog = spiral(matrix=matrix)
-    # prog = all_directions(matrix=matrix, left_ear=buttons.left, right_ear=buttons.right)
+    # prog = mad_spiral_until_left_button(matrix, buttons)
+    # prog = mickey(matrix=matrix, left_ear=buttons.left, right_ear=buttons.right)
+    # prog = all_directions(matrix, buttons)
+    # prog = spiral(matrix, buttons)
+    # prog = blackhole(matrix, buttons)
+    prog = blackhole_spiral(matrix, buttons)
 
     context = contextvars.copy_context()
     async with asyncio.TaskGroup() as tg:
--- blink/glt2024.py
+++ blink/glt2024.py
@@ -1,4 +1,4 @@
-from .blink import program, launch, sequence, cycle, blink, all, any, walk, forever, sleep
+from .blink import program, launch, sequence, cycle, blink, all, any, walk, forever, sleep, on, repeat
 from .io import Input, Output
 
 from gpiod import request_lines, LineSettings
@@ -11,9 +11,6 @@ OUTPUTS = (
     ( 3, 10,  6,  8, 15),
     ( 2, 22,  5, 25, 14),
 )
-
-INPUTS = (19, 26)
-
 
 class GLTMatrix:
     def __init__(self):
@@ -41,6 +38,9 @@ class GLTMatrix:
 
     def col(self, colno):
         return [self.matrix[rowno][colno] for rowno in range(len(self.matrix))]
+
+    def all(self):
+        return sum(self.matrix, [])
 
     def get(self, x, y):
         return self.matrix[x][y]
@@ -77,8 +77,8 @@ class GLTMatrix:
 
 class GLTButtons:
     def __init__(self):
-        self.left = Input('/dev/gpiochip0', INPUTS[0]);
-        self.right = Input('/dev/gpiochip0', INPUTS[1]);
+        self.left = Input('/dev/gpiochip0', 26);
+        self.right = Input('/dev/gpiochip0', 19);
     
 
 @program
@@ -126,13 +126,19 @@ async def cycle_cols(matrix):
     await prog()
 
 @program
-async def spiral(matrix):
-    prog = forever(
-        sequence(
-            walk(matrix.outer_ring_clockwise(), 0.07),
-            walk(matrix.inner_ring_clockwise(), 0.10),
-            blink(matrix.get(2,2), 0.01),
-        )
+async def spiral(matrix, buttons):
+    prog = any(
+        forever(
+            sequence(
+                walk(matrix.outer_ring_clockwise(), 0.07),
+                walk(matrix.inner_ring_clockwise(), 0.10),
+                any(
+                    blink(matrix.get(2,2), 0.01),
+                    wait_button(buttons.left),
+                ),
+            )
+        ),
+        wait_button(buttons.right),
     )
     await prog()
 
@@ -194,3 +200,273 @@ async def mad_spiral_until_left_button(matrix, buttons):
         ),
     )
     await prog()
+
+@program
+async def mickey(matrix, left_ear, right_ear):
+    left_eye_full = Output.from_many(
+        (
+            matrix.get(0,0),
+            matrix.get(0,1),
+            matrix.get(0,2),
+            matrix.get(1,0),
+            matrix.get(1,1),
+            matrix.get(1,2),
+            matrix.get(2,0),
+            matrix.get(2,1),
+            matrix.get(2,2),
+        ))
+    left_eye_cross = Output.from_many(
+        (
+            matrix.get(0,0),
+            matrix.get(0,2),
+            matrix.get(1,1),
+            matrix.get(2,0),
+            matrix.get(2,2),
+        ))
+    left_eye_dot = matrix.get(1,1)
+    right_eye_full = Output.from_many(
+        (
+            matrix.get(0,2),
+            matrix.get(0,3),
+            matrix.get(0,4),
+            matrix.get(1,2),
+            matrix.get(1,3),
+            matrix.get(1,4),
+            matrix.get(2,2),
+            matrix.get(2,3),
+            matrix.get(2,4),
+        ))
+    right_eye_cross = Output.from_many(
+        (
+            matrix.get(0,2),
+            matrix.get(0,4),
+            matrix.get(1,3),
+            matrix.get(2,2),
+            matrix.get(2,4),
+        ))
+    right_eye_dot = matrix.get(1, 3)
+
+    mouth_up = Output.from_many(
+        (
+            matrix.get(3,0),
+            matrix.get(4,1),
+            matrix.get(4,2),
+            matrix.get(4,3),
+            matrix.get(3,4),
+        ))
+    mouth_straight = Output.from_many(
+        (
+            matrix.get(4,0),
+            matrix.get(4,1),
+            matrix.get(4,2),
+            matrix.get(4,3),
+            matrix.get(4,4),
+        ))
+    mouth_down = Output.from_many(
+        (
+            matrix.get(4,0),
+            matrix.get(3,1),
+            matrix.get(3,2),
+            matrix.get(3,3),
+            matrix.get(4,4),
+        ))
+
+    await launch(
+        sequence(
+            any(
+                on(mouth_up),
+                any(
+                    repeat(
+                        sequence(                
+                            any(
+                                blink(left_eye_full, interval=0.5),
+                                wait_button(left_ear),
+                            ),
+                            any(
+                                blink(left_eye_dot, interval=0.5),
+                                wait_button(left_ear),
+                            ),
+                        ),
+                        ntimes=3
+                    ),
+                    repeat(
+                        sequence(                
+                            sleep(0.5),
+                            any(
+                                blink(right_eye_full, interval=0.5),
+                                wait_button(right_ear),
+                            ),
+                            any(
+                                blink(right_eye_dot, interval=0.5),
+                                wait_button(right_ear),
+                            ),
+                        ),
+                        ntimes=3
+                    ),
+                ),
+            ),
+            any(
+                on(mouth_straight),
+                all(
+                    any(
+                        on(left_eye_dot),
+                        wait_button(left_ear),
+                    ),
+                    any(
+                        on(right_eye_dot),
+                        wait_button(right_ear),
+                    ),
+                ),
+            ),
+            any(
+                on(mouth_straight),
+                all(
+                    on(left_eye_cross),
+                    on(right_eye_cross),
+                ),
+                all(
+                    wait_button(left_ear),
+                    wait_button(right_ear),
+                )
+            ),
+            all(
+                on(left_eye_cross),
+                on(right_eye_cross),
+                on(mouth_down),
+            ),
+        )
+    )
+
+@program
+async def all_directions(matrix, buttons):
+    await launch(
+        all(
+            forever(
+                sequence(
+                    any(
+                        cycle(matrix.outer_ring_clockwise(), interval=0.1),
+                        wait_button(buttons.left),
+                    ),
+                    any(
+                        cycle(list(reversed(matrix.outer_ring_clockwise())), interval=0.1),
+                        wait_button(buttons.left),
+                    ),
+                )
+            ),
+            forever(
+                sequence(
+                    any(
+                        cycle(matrix.inner_ring_clockwise(), interval=0.2),
+                        wait_button(buttons.right),
+                    ),
+                    any(
+                        cycle(list(reversed(matrix.inner_ring_clockwise())), interval=0.2),
+                        wait_button(buttons.right),
+                    ),
+                )
+            ),
+        )
+    )
+
+@program
+async def blackhole(matrix, buttons):
+    inner = matrix.get(2,2)
+    middle = Output.from_many(matrix.inner_ring_clockwise())
+    outer = Output.from_many(matrix.outer_ring_clockwise())
+
+    prog = sequence(
+        any(
+            forever(
+                sequence(
+                    any(
+                        cycle([inner, middle, outer], interval=0.4),
+                        wait_button(buttons.left),
+                    ),
+                    any(
+                        cycle([outer, middle, inner], interval=0.4),
+                        wait_button(buttons.left),
+                    ),
+                )
+            ),
+            wait_button(buttons.right),
+        ),
+        blink(Output.from_many(matrix.all()), interval=0.04, ntimes=15),
+    )
+
+    await prog()
+
+@program
+async def _walk_outer(matrix, clockwise, interval):
+    if clockwise:
+        outputs = matrix.outer_ring_clockwise()
+    else:
+        outputs = list(reversed(matrix.outer_ring_clockwise()))
+        
+    prog = walk(outputs, interval=interval)
+    await prog()
+
+@program
+async def _walk_inner(matrix, clockwise, interval):
+    if clockwise:
+        outputs = matrix.inner_ring_clockwise()
+    else:
+        outputs = list(reversed(matrix.inner_ring_clockwise()))
+        
+    prog = walk(outputs, interval=interval)
+    await prog()
+
+@program
+async def blackhole_spiral(matrix, buttons):
+    inner = matrix.get(2,2)
+
+    prog = sequence(
+        any(
+            forever(
+                sequence(
+                    any(
+                        forever(
+                            sequence(
+                                any(
+                                    blink(inner, interval=0.05),
+                                    sleep(0.5),
+                                ),
+                                any(
+                                    _walk_inner(matrix, clockwise=True, interval=0.05),
+                                    sleep(1),
+                                ),
+                                any(
+                                    _walk_outer(matrix, clockwise=True, interval=0.05),
+                                    sleep(1),
+                                ),
+                            ),
+                        ),
+                        wait_button(buttons.left),
+                    ),
+                    any(
+                        forever(
+                            sequence(
+                                any(
+                                    _walk_outer(matrix, clockwise=False, interval=0.05),
+                                    sleep(1),
+                                ),
+                                any(
+                                    _walk_inner(matrix, clockwise=False, interval=0.05),
+                                    sleep(1),
+                                ),
+                                any(
+                                    blink(inner, interval=0.05),
+                                    sleep(0.5),
+                                ),
+                            ),
+                        ),
+                        wait_button(buttons.left),
+                    ),
+                )
+            ),
+            wait_button(buttons.right),
+        ),
+        blink(Output.from_many(matrix.all()), interval=0.04, ntimes=15),
+    )
+
+    await prog()
+
